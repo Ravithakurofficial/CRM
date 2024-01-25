3341b335d803c9563a2d81bacdd286d44609437a
deepusuryan <deepusuryan11@gmail.com>
2024-01-25 12:31:20 +0530

    problem solvd by ravi

--- src/main/java/com/curd/firebase/runner.java
+++ src/main/java/com/curd/firebase/runner.java
@@ -6,8 +6,6 @@ import com.google.firebase.FirebaseOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Objects;
@@ -15,15 +13,13 @@ import java.util.Objects;
 @SpringBootApplication
 public class runner {
 
-    public static void main(String[] args) throws IOException {
-        ClassLoader classLoader = runner.class.getClassLoader();
+    public static void main(String[] args) {
+        try (InputStream serviceAccount = runner.class.getResourceAsStream("/serviceAccountKey.json")) {
+            if (serviceAccount == null) {
+                throw new IOException("Service account key not found.");
+            }
 
-
-        File file = new File(Objects.requireNonNull(classLoader.getResource("serviceAccountKey.json")).getFile());
-
-        // Use try-with-resources to automatically close the InputStream
-        try (InputStream serviceAccount = new FileInputStream(file)) {
-            FirebaseOptions options = new FirebaseOptions.Builder()
+            FirebaseOptions options = FirebaseOptions.builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                     .setDatabaseUrl("https://crm-database-8ee82-default-rtdb.firebaseio.com")
                     .build();
