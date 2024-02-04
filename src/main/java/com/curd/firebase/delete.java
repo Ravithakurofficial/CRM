9b22de73f18c45f0992d052c5ca37ed119cffa14
Ravi kumar Thakur <thakurravikumar400@gmail.com>
2024-02-04 19:11:32 +0530

    edit and delete both working

--- src/main/java/com/curd/firebase/delete.java
+++ src/main/java/com/curd/firebase/delete.java
@@ -7,24 +7,20 @@ import com.google.firebase.cloud.FirestoreClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.concurrent.ExecutionException;
 
 @RestController
 public class delete {
 
-    @DeleteMapping("/delete/{documentId}")
+    @DeleteMapping("/documents/{documentId}")
     public String deleteDocument(@PathVariable String documentId) {
-        try {
-            Firestore dbFirestore = FirestoreClient.getFirestore();
-            ApiFuture<WriteResult> deleteFuture = dbFirestore.collection("CRM").document(documentId).delete();
-
-            // Wait for the deletion operation to complete
-            deleteFuture.get();
-
-            return "Document with ID: " + documentId + " deleted successfully."; // Return success message
-        } catch (InterruptedException | ExecutionException e) {
-            e.printStackTrace();
-            return "Error deleting document: " + e.getMessage(); // Return error message
+        {
+            try {
+                Firestore dbFirestore = FirestoreClient.getFirestore();
+                ApiFuture<WriteResult> collectionsApiFuture = dbFirestore.collection("CRM").document(documentId).delete();
+                return "Successfully deleted " + documentId;
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 }
